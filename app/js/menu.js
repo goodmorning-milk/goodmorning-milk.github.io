3bea52f7224146526c950cf064f962726dd3b3a8
Nelson Chen <nechen@ap.corp.jwt.com>
2017-02-14 12:51:47 +0800

    0213

--- app/js/menu.js
+++ app/js/menu.js
@@ -7,6 +7,39 @@
 /*global app, $ */
 app.partial.menu = function(){
 	$('header a.burger').on('click', function(){
-		$('header nav').toggleClass('on');
+		if( !$('header nav').hasClass('on') ){
+			$('header nav').addClass('on');
+		}else{
+			$('header nav').removeClass('on');
+		}
+	});
+
+	$('header nav a:eq(0)').on('click', function(){
+		TweenMax.to('html, body', 0.5, {
+			scrollTop: 0
+		});
+	});
+
+	$('header nav a:eq(1)').on('click', function(){
+		TweenMax.to('html, body', 0.5, {
+			scrollTop: $('.videos .video:eq(0)').offset().top - 44
+		});
+	});
+
+	var videoNav = $('.nav-container');
+	videoNav.find('a:eq(0), a:eq(1)').on('click', () => {
+		TweenMax.to('html, body', 0.5, {
+			scrollTop: $('.videos .video:eq(0)').offset().top - 44
+		});
+	});
+	videoNav.find('a:eq(2), a:eq(3)').on('click', () => {
+		TweenMax.to('html, body', 0.5, {
+			scrollTop: $('.videos .video:eq(2)').offset().top - 44
+		});
+	});
+	videoNav.find('a:eq(4), a:eq(5)').on('click', () => {
+		TweenMax.to('html, body', 0.5, {
+			scrollTop: $('.videos .video:eq(4)').offset().top - 44
+		});
 	});
 };
